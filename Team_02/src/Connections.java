25ea3005341b8bd9fde8fdbaa82ff8b9759f551e
surya-de <schatt37@asu.edu>
2020-02-19 21:09:29 -0700

    Added check validation module

--- Team_02/src/Connections.java
+++ Team_02/src/Connections.java
@@ -16,7 +16,7 @@ import java.util.ArrayList;
 
 
 class Connections extends JFrame{
-	ArrayList<Point> moves = new ArrayList<Point>();
+	static ArrayList<Point> moves = new ArrayList<Point>();
 	ShapeLocation objShape = new ShapeLocation();
     public Connections(){
         
@@ -24,30 +24,30 @@ class Connections extends JFrame{
 
     public void DotToDotConnection(Graphics g, int x1, int y1, int x2, int y2)
     {
-    	
+    	boolean IsAvailable = false;
             try{
             	if (moves.size() == 0){
                 	moves.add(new java.awt.Point(x1, y1));
                 	moves.add(new java.awt.Point(x2, y2));
                 	Lineconnection objLineConnect = new Lineconnection(new Point(x1, y1), new Point(x2, y2));
                 	ShapeLocation.LinePoint.add(objLineConnect);
-                	//drawLines(g, x1, y1, x2, y2);
                 	
             	}
             	else {
                 	for (int i = 0; i < moves.size(); i ++) {
-                		if ((moves.get(i).x == x1 || moves.get(i).y == y1) &&
-                				(moves.get(i).x == x2 || moves.get(i).y == y2)) {
+                		if ((moves.get(i).x == x1 && moves.get(i).y == y1) ||
+                				(moves.get(i).x == x2 && moves.get(i).y == y2)) {
                 			System.out.println("Already available");
+                			IsAvailable = true;
                 		}
-                		else {
-                        	moves.add(new java.awt.Point(x1, y1));
-                        	moves.add(new java.awt.Point(x2, y2));
-                        	Lineconnection objLineConnect = new Lineconnection(new Point(x1, y1), new Point(x2, y2));
-                        	ShapeLocation.LinePoint.add(objLineConnect);
-                        	//drawLines(g, x1, y1, x2, y2);
-                		}
+
                 	}	
+                	if (IsAvailable == false) {
+                    	moves.add(new java.awt.Point(x1, y1));
+                    	moves.add(new java.awt.Point(x2, y2));
+                    	Lineconnection objLineConnect = new Lineconnection(new Point(x1, y1), new Point(x2, y2));
+                    	ShapeLocation.LinePoint.add(objLineConnect);
+                	}
             	}
             	
             }
